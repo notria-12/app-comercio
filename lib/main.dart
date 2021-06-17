5191dc2994af8256fea5e1586a3bc5abc553d716
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-06-17 17:00:03 -0300

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/views/Home/Home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // final FirebaseApp app = await Firebase.initializeApp();
   runApp(MyApp());
 }
 
