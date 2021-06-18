394c4bb4bbbbfcdb3d867362993f3187efa44c8d
Airton Araujo <airton.araujo.s18@gmail.com>
2021-06-18 13:32:13 -0300

    add model ProductService

---
+++ lib/src/models/ProductService.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+class ProductService {
+  String _imagePath;
+  String _name;
+  String _address;
+
+  ProductService(this._imagePath, this._name, this._address);
+
+  // ProductService copyWith({
+  //   String? _imagePath,
+  //   String? _name,
+  // }) {
+  //   return ProductService(
+  //     _imagePath ?? this._imagePath,
+  //     _name ?? this._name,
+  //   );
+  // }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'imagePath': _imagePath,
+      'name': _name,
+    };
+  }
+
+  factory ProductService.fromMap(Map<String, dynamic> map) {
+    return ProductService(
+      map['_imagePath'],
+      map['_name'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductService.fromJson(String source) =>
+      ProductService.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'ProductService(_imagePath: $_imagePath, _name: $_name)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ProductService &&
+        other._imagePath == _imagePath &&
+        other._name == _name;
+  }
+
+  @override
+  int get hashCode => _imagePath.hashCode ^ _name.hashCode;
+}
